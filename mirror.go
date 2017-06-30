9d2b8e0dead923be0ca73ff0c893efc1b755c5aa
janczer <menshikov.ivn@gmail.com>
2017-06-30 10:37:39 +0200

    Fix function

--- mirror.go
+++ mirror.go
@@ -6,15 +6,15 @@ func MirrorImage(in image.Image, ox bool, oy bool) image.Image {
 	bounds := in.Bounds()
 	out := image.NewRGBA(bounds)
 
-	for y := bounds.Min.Y; y <= bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x <= bounds.Max.X; x++ {
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			c := in.At(x, y)
 			nx, ny := x, y
 			if ox {
-				nx = bounds.Max.X - x - 1
+				nx = bounds.Max.X - 1 - x
 			}
 			if oy {
-				ny = bounds.Max.Y - y - 1
+				ny = bounds.Max.Y - 1 - y
 			}
 			out.Set(nx, ny, c)
 		}
